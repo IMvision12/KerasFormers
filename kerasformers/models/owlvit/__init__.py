20e55f95da6e313ac5c6091bf400eb896c20abab
Gitesh <88665786+IMvision12@users.noreply.github.com>
2026-05-18 20:02:50 -0700

    Add OwlV2 (#243)

--- kerasformers/models/owlvit/__init__.py
+++ kerasformers/models/owlvit/__init__.py
@@ -2,13 +2,13 @@ from .owlvit_image_processor import (
     OwlViTImageProcessor,
     owlvit_post_process_object_detection,
 )
-from .owlvit_model import OwlViT, OwlViTDetect
+from .owlvit_model import OwlViTDetect, OwlViTModel
 from .owlvit_processor import OwlViTProcessor
 
 __all__ = [
-    "OwlViT",
     "OwlViTDetect",
     "OwlViTImageProcessor",
+    "OwlViTModel",
     "OwlViTProcessor",
     "owlvit_post_process_object_detection",
 ]
