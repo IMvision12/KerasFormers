e2b87b35a4a93a6ca90c4417533ac81521fd7bae
IMvision12 <gitesh.ch.0912@gmail.com>
2026-05-29 21:23:30 -0700

    fix

--- kerasformers/models/qwen2_5_vl/__init__.py
+++ kerasformers/models/qwen2_5_vl/__init__.py
@@ -1,6 +1,7 @@
 from kerasformers.models.qwen2_5_vl.qwen2_5_vl_model import (
     Qwen2_5_VLGenerate,
     Qwen2_5_VLModel,
+    Qwen2_5_VLTextModel,
     Qwen2_5_VLVisionModel,
 )
 from kerasformers.models.qwen2_vl.qwen2_vl_processor import (
@@ -10,6 +11,7 @@ from kerasformers.models.qwen2_vl.qwen2_vl_processor import (
 __all__ = [
     "Qwen2_5_VLModel",
     "Qwen2_5_VLGenerate",
+    "Qwen2_5_VLTextModel",
     "Qwen2_5_VLVisionModel",
     "Qwen2_5_VLProcessor",
 ]
