6b965edcbf2d274597090c324e3cd664b5b8f6b4
IMvision12 <gitesh.ch.0912@gmail.com>
2026-06-19 23:53:47 -0700

    Add GLM 5

---
+++ kerasformers/models/glm5_moe/__init__.py
@@ -0,0 +1,11 @@
+from kerasformers.models.glm5_moe.glm5_moe_model import (
+    Glm5MoeGenerate,
+    Glm5MoeModel,
+)
+from kerasformers.models.glm5_moe.glm5_moe_tokenizer import Glm5MoeTokenizer
+
+__all__ = [
+    "Glm5MoeModel",
+    "Glm5MoeGenerate",
+    "Glm5MoeTokenizer",
+]
