fafffa0976a011d04b09990e4b953e2425d3074b
Gitesh <88665786+IMvision12@users.noreply.github.com>
2026-08-11 23:09:01 -0700

    Add Qwen2 Weights and config (#367)

--- kerasformers/models/qwen2/__init__.py
+++ kerasformers/models/qwen2/__init__.py
@@ -1,4 +1,5 @@
+from kerasformers.models.qwen2.qwen2_config import Qwen2Config
 from kerasformers.models.qwen2.qwen2_model import Qwen2Generate, Qwen2Model
 from kerasformers.models.qwen2.qwen2_tokenizer import Qwen2Tokenizer
 
-__all__ = ["Qwen2Model", "Qwen2Generate", "Qwen2Tokenizer"]
+__all__ = ["Qwen2Model", "Qwen2Generate", "Qwen2Config", "Qwen2Tokenizer"]
